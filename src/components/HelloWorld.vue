5b19c03db3475c9a5ebd396f972fa3873bd56293
Sylvain Lesage <severo@rednegra.net>
2019-10-18 10:29:15 +0200

    build: remove decorators

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -35,12 +35,14 @@
 </template>
 
 <script lang="ts">
-import { Component, Prop, Vue } from 'vue-property-decorator'
+import Vue from 'vue'
 
-@Component
-export default class HelloWorld extends Vue {
-  @Prop() private msg!: string;
-}
+export default Vue.extend({
+  name: 'HelloWorld',
+  props: {
+    msg: String
+  }
+})
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
