4b5264cfb2357d19d304c527f6c4f6b806cc017e
Sylvain Lesage <severo@rednegra.net>
2019-10-31 11:25:47 +0100

    feat: add SVG layer
    Reference coordinates are CSS px.
    SVG only uses CSS px, while canvas scales depending on devicePixelRatio.
    
    It means that the number of pixels in the canvas will be higher on HD or
    retina screens (eg. every CSS pixel is rendered by 4 or 9 physical pixels)
    When a browser zoom is applied, the number of pixels in the canvas will
    also be adapted. But as the image takes all the screen (100vh for
    <v-content>), the apparent resolution stays unchanged when zooming or
    unzooming.

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -9,17 +9,34 @@
       no-gutters
       justify="center"
     >
-      <v-skeleton-loader
-        class="mx-auto"
-        min-width="300"
-        type="card"
-        v-show="showPlaceholder"
-      />
-      <canvas
-        id="image-canvas"
-        ref="imagecanvas"
-        v-show="showCanvas"
-      />
+      <div id="image-wrapper">
+        <v-skeleton-loader
+          class="mx-auto"
+          min-width="300"
+          type="card"
+          v-show="showPlaceholder"
+        />
+        <canvas
+          id="image-canvas"
+          ref="imagecanvas"
+          v-show="showCanvas"
+          :width="canvasWidth"
+          :height="canvasHeight"
+          :style="canvasStyle"
+        />
+        <svg
+          id="image-svg"
+          ref="imagesvg"
+          v-show="showSvg"
+          :viewBox="viewbox"
+          :style="svgStyle"
+        ><circle
+          :cx="cx"
+          :cy="cy"
+          r="10"
+        />
+        </svg>
+      </div>
     </v-row>
   </v-container>
 </template>
@@ -43,7 +60,10 @@ export default {
         'bazzania'
       ],
       loadedImage: new Image(),
-      debug: false
+      debug: false,
+      width: 300, // in CSS px
+      height: 150, // in CSS px
+      devicePixelRatio: 1
     }
   },
   computed: {
@@ -56,6 +76,9 @@ export default {
     ctx: function () {
       return this.canvas.getContext('2d')
     },
+    svg: function () {
+      return this.$refs.imagesvg
+    },
     container: function () {
       return this.$refs.container
     },
@@ -65,36 +88,68 @@ export default {
     showCanvas: function () {
       return !this.isResizing
     },
+    showSvg: function () {
+      return !this.isResizing
+    },
     name: function () {
       return this.names[this.image]
     },
     maxImageWidth: function () {
       return this.imageWidths[this.imageWidths.length - 1]
     },
+    canvasWidth: function () {
+      return this.width * this.devicePixelRatio
+    },
+    canvasHeight: function () {
+      return this.height * this.devicePixelRatio
+    },
+    canvasStyle: function () {
+      return {
+        width: `${this.width}px`,
+        height: `${this.height}px`
+      }
+    },
+    viewbox: function () {
+      return `0 0 ${this.width} ${this.height}`
+    },
+    svgStyle: function () {
+      return this.canvasStyle
+    },
+    cx: function () {
+      return this.width / 2
+    },
+    cy: function () {
+      return this.height / 2
+    },
     ...mapState(['image'])
   },
   methods: {
+    dprChanged: function () {
+      if (this.devicePixelRatio !== window.devicePixelRatio) {
+        this.devicePixelRatio = window.devicePixelRatio
+        return true
+      }
+      return false
+    },
     resize: function () {
       // Adapted from https://webglfundamentals.org/webgl/lessons/webgl-anti-patterns.html
-      const scale = window.devicePixelRatio
-      let width = this.container.clientWidth * scale
-      let height = this.container.clientHeight * scale
+      let width = this.container.clientWidth
+      let height = this.container.clientHeight
       if (width > height * this.aspectRatio) {
         width = Math.floor(height * this.aspectRatio)
       } else {
         height = Math.floor(width / this.aspectRatio)
       }
-      if (this.canvas.width !== width || this.canvas.height !== height) {
-        this.canvas.width = width
-        this.canvas.height = height
-        this.canvas.style.width = (width / scale) + 'px'
-        this.canvas.style.height = (height / scale) + 'px'
+      if (this.width !== width || this.height !== height) {
+        this.width = width
+        this.height = height
         return true
       }
       return false
     },
     checkRender: function () {
-      if (this.resize() || this.needToRender) {
+      // Note: we could check the devicePixelRatio value (dprChanged) at a lower rate if it affects the performance
+      if (this.resize() || this.dprChanged() || this.needToRender) {
         this.needToRender = false
 
         this.isResizing = true
@@ -109,32 +164,33 @@ export default {
     },
     drawStuff: function () {
       // Redraw & reposition content
-      // ctx.scale(scale, scale);
-      this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height)
-      this.ctx.drawImage(this.loadedImage, 0, 0, this.canvas.width, this.canvas.height)
+      this.ctx.scale(this.devicePixelRatio, this.devicePixelRatio)
+      this.ctx.clearRect(0, 0, this.width, this.height)
+      this.ctx.drawImage(this.loadedImage, 0, 0, this.width, this.height)
 
       if (this.debug) {
         const resizeText = 'Canvas width: ' + this.canvas.width + 'px' + ' - image: ' + this.name
         this.ctx.textAlign = 'center'
         this.ctx.fillStyle = '#000'
-        this.ctx.fillText(resizeText, this.canvas.width / 2, this.canvas.height / 2)
+        this.ctx.fillText(resizeText, this.width / 2, this.height / 2)
       }
     },
     getImageFilename: function (name, w) {
       return `https://github.com/severo/pictures/raw/master/images,w_${w}/${name}.jpg`
     },
-    test: function (img) {
+    updateFromLoadedImage: function (img) {
       this.loadedImage = img
       this.needToRender = true
     },
     createImage: function () {
       const img = new Image()
-      img.onload = () => this.test(img)
+      img.onload = () => this.updateFromLoadedImage(img)
       img.src = this.getImageFilename(this.name, this.maxImageWidth)
       img.srcset = this.imageWidths.map(w => `${this.getImageFilename(this.name, w)} ${w}w`).join(',')
     }
   },
   mounted: function () {
+    this.devicePixelRatio = window.devicePixelRatio
     this.createImage()
     this.checkRender()
   },
@@ -147,4 +203,11 @@ export default {
 </script>
 
 <style lang="sass">
+  div#image-wrapper
+    position: relative
+  svg#image-svg
+    position: absolute
+    top: 0
+    left: 0
+    z-index: 2
 </style>
