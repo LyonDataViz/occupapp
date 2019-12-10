4c5805022b76e1b1331b59658638f352e71c1bf9
Sylvain Lesage <severo@rednegra.net>
2019-12-10 13:59:36 +0100

    feat: add information about feedback and credits
    fixes https://github.com/LyonDataViz/occupapp/issues/24

--- src/components/Infos.vue
+++ src/components/Infos.vue
@@ -1,11 +1,33 @@
 <template>
-  <div class="pa-2">
-    <span class="overline">v{{ appVersion }}</span><span class="caption">. <a href="https://github.com/LyonDataViz/occupapp/blob/master/VERSIONS.md">See more</a> about versions.</span>
+  <div class="pa-2 caption">
+    <p>
+      Want to give us a feedback or a suggestion? Please fill an <a
+        href="https://github.com/LyonDataViz/occupapp/issues/new"
+      >issue</a>.
+    </p>
+    <p class="credits">
+      <img
+        src="/liris.png"
+        height
+      > Developed for the <a
+        href="https://projet.liris.cnrs.fr/mi2/"
+      >M2i</a> project (<a
+        href="https://github.com/LyonDataViz/occupapp/blob/master/VERSIONS.md"
+      >v{{ appVersion }}</a>)
+    </p>
   </div>
 </template>
 
 <style scoped>
-
+.credits {
+  text-align: center;
+  margin: 0;
+}
+img {
+  vertical-align: middle;
+  height: 1rem;
+  width: 1rem;
+}
 </style>
 
 <script lang="ts">
