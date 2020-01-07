9c117997b7eba85242c6e0533fcd31d0d9d5f94b
Sylvain Lesage <severo@rednegra.net>
2020-01-07 12:36:07 +0100

    fix: create version info even without git

--- vue.config.js
+++ vue.config.js
@@ -5,9 +5,20 @@ const gitRevisionPlugin = new GitRevisionPlugin()
 const appVersion = package.version
 const appVersionUrl = `https://github.com/LyonDataViz/occupapp/blob/master/VERSIONS.md#v${appVersion.replace(/\./g, '')}`
 const gitRepositoryBaseUrl = package.repository.url.replace(/\.[^/.]+$/, '')
-const gitVersion = gitRevisionPlugin.version()
-const gitCommitHash = gitRevisionPlugin.commithash()
-const gitCommitUrl = `${gitRepositoryBaseUrl}/commit/${gitCommitHash}`
+
+let gitVersion
+let gitCommitHash
+try {
+  gitVersion = gitRevisionPlugin.version()
+  gitCommitHash = gitRevisionPlugin.commithash()
+} catch (e) {
+  // Must be a deployment made from an export of the code, without .git
+  // That's what does now.sh
+  // They provide environment variables (https://zeit.co/docs/v2/more/now-for-github#included-environment-variables)
+  // See below how they are used.
+}
+
+const getGitCommitUrl = commitHash => `${gitRepositoryBaseUrl}/commit/${gitCommitHash}`
 
 module.exports = {
   'chainWebpack': config => {
@@ -15,8 +26,14 @@ module.exports = {
     config.plugin('define').tap(args => {
       args[0]['process.env']['INFOS_APP_VERSION'] = JSON.stringify(appVersion)
       args[0]['process.env']['INFOS_APP_VERSION_URL'] = JSON.stringify(appVersionUrl)
-      args[0]['process.env']['INFOS_GIT_VERSION'] = JSON.stringify(gitVersion)
-      args[0]['process.env']['INFOS_GIT_VERSION_URL'] = JSON.stringify(gitCommitUrl)
+
+      if (gitVersion !== undefined && gitCommitHash !== undefined) {
+        args[0]['process.env']['INFOS_GIT_VERSION'] = JSON.stringify(gitVersion)
+        args[0]['process.env']['INFOS_GIT_VERSION_URL'] = JSON.stringify(getGitCommitUrl(gitCommitHash))
+      } else if ('NOW_GITHUB_COMMIT_SHA' in args[0]['process.env']) {
+        args[0]['process.env']['INFOS_GIT_VERSION'] = JSON.stringify(args[0]['process.env']['NOW_GITHUB_COMMIT_SHA'].slice(0, 8))
+        args[0]['process.env']['INFOS_GIT_VERSION_URL'] = JSON.stringify(getGitCommitUrl(args[0]['process.env']['NOW_GITHUB_COMMIT_SHA']))
+      }
       return args
     })
   },
