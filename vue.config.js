cbd9cd5790a8ed0a8b404dc70e436a7ba6137766
Sylvain Lesage <severo@rednegra.net>
2020-01-07 15:13:56 +0100

    fix: fix use of now.sh environment variables

--- vue.config.js
+++ vue.config.js
@@ -2,23 +2,35 @@ const package = require('./package.json')
 
 const appVersion = package.version
 const appVersionUrl = `https://github.com/LyonDataViz/occupapp/blob/master/VERSIONS.md#v${appVersion.replace(/\./g, '')}`
-const gitRepositoryBaseUrl = package.repository.url.replace(/\.[^/.]+$/, '')
+const gitRepositoryBaseUrl = package.repository.url.replace(/\.git$/, '')
 
 let gitVersion
-let gitCommitHash
-try {
-  const GitRevisionPlugin = require('git-revision-webpack-plugin')
-  const gitRevisionPlugin = new GitRevisionPlugin()
-  gitVersion = gitRevisionPlugin.version()
-  gitCommitHash = gitRevisionPlugin.commithash()
-} catch (e) {
-  // Must be a deployment made from an export of the code, without .git
-  // That's what does now.sh
-  // They provide environment variables (https://zeit.co/docs/v2/more/now-for-github#included-environment-variables)
-  // See below how they are used.
-}
+let gitVersionUrl
+if ('NOW_GITHUB_DEPLOYMENT' in process.env) {
+  // See environment variables provided by now.sh
+  // https://zeit.co/docs/v2/more/now-for-github#included-environment-variables
+  const org = process.env.NOW_GITHUB_COMMIT_ORG || 'LyonDataViz'
+  const repo = process.env.NOW_GITHUB_COMMIT_REPO || 'occupapp'
+  const gitCommitHash = process.env.NOW_GITHUB_COMMIT_SHA
 
-const getGitCommitUrl = commitHash => `${gitRepositoryBaseUrl}/commit/${gitCommitHash}`
+  gitVersion = gitCommitHash.slice(0, 8)
+  gitVersionUrl = `https://github.com/${org}/${repo}/commit/${gitCommitHash}`
+} else {
+  try {
+    const GitRevisionPlugin = require('git-revision-webpack-plugin')
+    const gitRevisionPlugin = new GitRevisionPlugin()
+    const gitCommitHash = gitRevisionPlugin.commithash()
+
+    gitVersion = gitCommitHash.slice(0, 8)
+    gitVersionUrl = `${gitRepositoryBaseUrl}/commit/${gitCommitHash}`
+  } catch (e) {
+    // We must be in a case where
+    // - the code has been exported from the git repository
+    // - AND it's not being deployed on now.sh
+    gitVersion = 'unknown'
+    gitVersionUrl = gitRepositoryBaseUrl
+  }
+}
 
 module.exports = {
   'chainWebpack': config => {
@@ -26,14 +38,8 @@ module.exports = {
     config.plugin('define').tap(args => {
       args[0]['process.env']['INFOS_APP_VERSION'] = JSON.stringify(appVersion)
       args[0]['process.env']['INFOS_APP_VERSION_URL'] = JSON.stringify(appVersionUrl)
-
-      if (gitVersion !== undefined && gitCommitHash !== undefined) {
-        args[0]['process.env']['INFOS_GIT_VERSION'] = JSON.stringify(gitVersion)
-        args[0]['process.env']['INFOS_GIT_VERSION_URL'] = JSON.stringify(getGitCommitUrl(gitCommitHash))
-      } else if ('NOW_GITHUB_COMMIT_SHA' in args[0]['process.env']) {
-        args[0]['process.env']['INFOS_GIT_VERSION'] = JSON.stringify(args[0]['process.env']['NOW_GITHUB_COMMIT_SHA'].slice(0, 8))
-        args[0]['process.env']['INFOS_GIT_VERSION_URL'] = JSON.stringify(getGitCommitUrl(args[0]['process.env']['NOW_GITHUB_COMMIT_SHA']))
-      }
+      args[0]['process.env']['INFOS_GIT_VERSION'] = JSON.stringify(gitVersion)
+      args[0]['process.env']['INFOS_GIT_VERSION_URL'] = JSON.stringify(gitVersionUrl)
       return args
     })
   },
